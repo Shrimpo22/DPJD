bb7386adbdaca219fb7918eb68574cc6041dfc15
Pu4DinFL7 <flaviozmiguel27@gmail.com>
2024-10-29 13:59:27 +0000

    Bear Trap merged

--- Save the King/Assets/Scripts/BearTrap.cs
+++ Save the King/Assets/Scripts/BearTrap.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class BearTrap : MonoBehaviour
 {
     public Animator trapAnimator;  // Reference to the Animator component
+    public AudioSource trapSound;  // Reference to the AudioSource component
     private bool isTriggered = false;  // Prevents the trap from being triggered multiple times
 
+    void Start()
+    {
+        // Ensure the AudioSource component is assigned
+        if (trapSound == null)
+        {
+            trapSound = GetComponent<AudioSource>();
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
 
@@ -17,6 +26,9 @@ public class BearTrap : MonoBehaviour
             // Trigger the "CloseTrap" animation
             trapAnimator.SetTrigger("CloseTrap");
 
+            // Play the sound
+            trapSound.Play();
+
             //other.hp -= x , onde hp é o hp do player e x é o damage da trap.
 
             // Se trapped entao nao anda.
@@ -24,7 +36,6 @@ public class BearTrap : MonoBehaviour
 
             // Set the trap as triggered to prevent retriggering
             isTriggered = true;
-             Debug.Log("inside if");
         }
     }
 }

74bdd2ba216ffd68a182297df9efd3f194d26af3
Pu4DinFL7 <flaviozmiguel27@gmail.com>
2024-11-29 19:19:39 +0000

    Sons implementados no player e nos inimigos
    if(nobodyTalks){
     Instantiate PepGuardiola();
    }

--- Save the King/Assets/Scripts/Enemy/EnemyAnimSoundEvents.cs
+++ Save the King/Assets/Scripts/Enemy/EnemyAnimSoundEvents.cs
@@ -7,8 +7,8 @@ public class EnemyAnimSoundEvents : MonoBehaviour
     private AudioSource audioSource;
     private Animator animator;
 
-    [Header("AudioClips")]
-    public AudioClip playerSwordGrunt;
+    public EnemyAudioConfig audioConfig;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -18,6 +18,47 @@ public class EnemyAnimSoundEvents : MonoBehaviour
 
     public void PlayEnemyGetHitSound()
     {
+        if (audioConfig.playerSwordGrunt != null)
+            audioSource.PlayOneShot(audioConfig.playerSwordGrunt);
+    }
 
+    public void EnemyAttack()
+    {
+        if(audioConfig.attack1 != null)
+            audioSource.PlayOneShot(audioConfig.attack1);
+    }
+    
+    public void EnemyAttack2()
+    {
+        if(audioConfig.attack2 != null)
+            audioSource.PlayOneShot(audioConfig.attack2);
+    }
+
+    public void EnemyWalk()
+    {
+        float speed = animator.GetFloat("speed");
+        if (audioConfig.walkSound != null && speed > 0.5f)
+            Debug.Log("Estou a andar");
+        {
+            audioSource.PlayOneShot(audioConfig.walkSound);
+        }
+    }
+
+    public void EnemyGetHit()
+    {
+        if (audioConfig.hitSound != null)
+            audioSource.PlayOneShot(audioConfig.hitSound);
+    }    
+    
+    public void EnemyDeath()
+    {
+        if (audioConfig.deathSound != null)
+            audioSource.PlayOneShot(audioConfig.deathSound);
+    }
+
+    public void EnemyAttackSwing()
+    {
+        if(audioConfig.swingSound != null)
+            audioSource.PlayOneShot(audioConfig.swingSound);
     }
 }

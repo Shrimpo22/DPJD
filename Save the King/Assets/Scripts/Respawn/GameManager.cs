e44b68c69cd1b049d8978036060b6741e92855af
Bruno Carvalho <61797027+BrunixD@users.noreply.github.com>
2024-11-29 17:21:22 +0000

    guide

--- Save the King/Assets/Scripts/Respawn/GameManager.cs
+++ Save the King/Assets/Scripts/Respawn/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // HOW TO ADD THE RESPAWN AND ENEMY RESET
+    // Create 3 empty gameobjects, gamemanager to add this script,
+    //enemymanager to add the emptygameobject holding all enemies in the scene 
+    //and its script and respawnpoint for the respawnpoint script.
+    // Add the necessary references
     public static GameManager instance;
     public Transform respawnPoint;
 

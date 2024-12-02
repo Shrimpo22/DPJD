3e7f5839f3f528f0f8c6b9d504dbb2d99066b487
Bruno Carvalho <61797027+BrunixD@users.noreply.github.com>
2024-12-02 19:15:37 +0000

    Checkpoint
    seamlessly load scene done

--- Save the King/Assets/Scripts/SceneManagement/GameManager.cs
+++ Save the King/Assets/Scripts/SceneManagement/GameManager.cs
@@ -7,18 +7,17 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     public GameObject player;
     public GameObject playerHUD;
-    
+
     // Store player data like position and scene state
     public Dictionary<string, SceneState> sceneStates = new();
 
     private void Awake()
     {
-        // Registar o método no evento quando a cena for carregada
-        SceneManager.sceneLoaded += OnSceneLoaded;
+       // SceneManager.sceneLoaded += OnSceneLoaded;
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // Make sure this persists between scenes
+            DontDestroyOnLoad(gameObject);
             DontDestroyOnLoad(player);
             DontDestroyOnLoad(playerHUD);
    
@@ -37,91 +36,30 @@ public class GameManager : MonoBehaviour
     }
 
     // Esse método será chamado quando a nova cena for carregada
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        // Procurar o objeto de destino na nova cena
-        GameObject targetObject = GameObject.FindGameObjectWithTag("SpawnPoint"+scene.name);;
-        CharacterController controller = player.GetComponentInChildren<CharacterController>();
-        if (targetObject != null)
-        {
-            Debug.Log("SPAWN POSITION : " + targetObject.transform.position);
-            Debug.Log("Player POSITION : " + player.transform.position);
-            // Teletransportar o jogador para a posição do objeto
-            controller.enabled = false; // Disable temporarily to prevent conflicts
-            controller.transform.position = targetObject.transform.position;
-            //player.transform.position = targetObject.transform.position;
-            controller.enabled = true;
-        }
-    }
+    //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    //{
+   //     // Procurar o objeto de destino na nova cena
+    //    GameObject targetObject = GameObject.FindGameObjectWithTag("SpawnPoint"+scene.name);;
+    //    CharacterController controller = player.GetComponentInChildren<CharacterController>();
+    //    if (targetObject != null)
+    //    {
+    //        Debug.Log("SPAWN POSITION : " + targetObject.transform.position);
+    //        Debug.Log("Player POSITION : " + player.transform.position);
+    //        // Teletransportar o jogador para a posição do objeto
+    //        controller.enabled = false; // Disable temporarily to prevent conflicts
+    //        controller.transform.position = targetObject.transform.position;
+    //        //player.transform.position = targetObject.transform.position;
+    //        controller.enabled = true;
+    //    }
+   // }
 
-     private void OnDestroy()
-    {
-        // Desregistrar o evento ao destruir o objeto para evitar problemas
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-
+    // private void OnDestroy()
+    //{
+    //    // Desregistrar o evento ao destruir o objeto para evitar problemas
+    //    SceneManager.sceneLoaded -= OnSceneLoaded;
+    //}
 
 
-
-    public void SaveCurrentSceneState()
-    {
-        string currentScene = SceneManager.GetActiveScene().name;
-        SceneState state = new()
-        {
-            playerLastPosition = player.transform,
-            // Add more state data as needed
-            //playerGO = player, // save player gameobject;
-            // inventory
-            // enemies dead
-            // puzzles done
-        };
-        Instance.sceneStates[currentScene] = state;
-    }
-
-    public void LoadNewScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-
-        CharacterController controller = player.GetComponentInChildren<CharacterController>();
-
-        GameObject spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint"+sceneName);
-        Debug.Log("spawnPoint : " + spawnPoint);
-        
-        if (spawnPoint != null) {
-            Debug.Log("SPAWN POINT : " + spawnPoint.transform.position);
-            Vector3 spawnPosition = spawnPoint.transform.position;
-            Debug.Log("SPAWN POSITION : " + spawnPosition);
-            Debug.Log("CONTROLLER POSITION : " + controller.transform.position);
-            controller.enabled = false; // Disable temporarily to prevent conflicts
-            player.transform.position = spawnPosition;
-            controller.enabled = true;
-        
-        } else {
-            controller.enabled = false; // Disable temporarily to prevent conflicts
-            player.transform.position = new Vector3(0,0,0);
-            controller.enabled = true;
-        }
-
-        // If this scene was visited before, restore its state
-        if (Instance.sceneStates.ContainsKey(sceneName))
-        {
-
-            RestoreSceneState(sceneName);
-            Debug.Log("restored");
-            // Restore more state data as needed
-        }
-        
-    }
-
-    void RestoreSceneState(string sceneName)
-    {
-        if (Instance.sceneStates.ContainsKey(sceneName))
-        {
-            SceneState state = Instance.sceneStates[sceneName];
-            transform.position = state.playerLastPosition.position;
-        }
-    }
 
 }
 
